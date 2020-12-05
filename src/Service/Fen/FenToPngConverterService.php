c06122672b4fadb172d6e0aa954652ae07988361
nikola.mitrovic <nikola.mitrovic@soprex.com>
2020-12-05 21:10:25 +0100

    changed png provider

--- src/Service/Fen/FenToPngConverterService.php
+++ src/Service/Fen/FenToPngConverterService.php
@@ -4,9 +4,23 @@ namespace App\Service\Fen;
 
 class FenToPngConverterService implements FenConverterInterface
 {
+    private $fenFormatter;
+
+    public function __construct()
+    {
+        $this->fenFormatter = new FenFormatService();
+    }
+
     public function convert(string $fen)
     {
+        $colorToPlay = $this->fenFormatter->getColorToPlayFromFen($fen);
         $fen = explode(" ", $fen)[0];
-        return 'http://www.fen-to-image.com/image/100/' . $fen;
+        $png = 'https://chessboardimage.com/' . $fen;
+
+        if ($colorToPlay === 'black') {
+            $png.= '-flip';
+        }
+
+        return $png . '.png';
     }
 }
