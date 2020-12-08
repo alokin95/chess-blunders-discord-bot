9d7c2dd3455c951f964fbe6dcfa34af17f1534d5
nikola.mitrovic <nikola.mitrovic@soprex.com>
2020-12-08 15:18:44 +0100

    changed response message

--- src/Response/BlunderSolvedResponse.php
+++ src/Response/BlunderSolvedResponse.php
@@ -8,6 +8,6 @@ class BlunderSolvedResponse extends AbstractResponse
 {
     protected function sendResponse()
     {
-        $this->message->author->sendMessage('Congratulations!');
+        $this->message->author->sendMessage("Congratulations! You've solved the blunder!");
     }
 }
